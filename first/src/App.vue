0e8b83d80eb8ea77df66f34e8981662d87765e8a
Marco Sandri <sandrimarco98@gmail.com>
2022-02-18 16:07:40 +0100

    vue cli first

---
+++ first/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <TitleComponent></TitleComponent>
+    <HeroComponent></HeroComponent>
+  </div>
+</template>
+
+<script>
+import TitleComponent from './components/TitleComponent.vue'
+import HeroComponent from './components/HeroComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TitleComponent,
+    HeroComponent
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  margin:0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #0080ff;
+  margin-top: 60px;
+}
+</style>
---
+++ first/src/components/HeroComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="fullpage">
+        <img src="https://picsum.photos/1200/500">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HeroComponent'
+    }
+</script>
+
+<style scoped>
+    .fullpage{
+        width: 100vw;
+        height: 500px;
+    }
+    img{
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+    }
+</style>
